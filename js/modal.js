b64b4b8c383e82fb57ccfe61e372c602306db33a
Valerii Pometun <vmpometun@gmail.com>
2023-09-28 15:20:17 +0300

    add open modal by different buttons

--- js/modal.js
+++ js/modal.js
@@ -1,16 +1,18 @@
 (() => {
   const refs = {
-    openModalBtn: document.querySelector('[data-modal-open]'),
+    openModalBtn: document.querySelectorAll('[data-modal-open]'),
     closeModalBtn: document.querySelector('[data-modal-close]'),
     modal: document.querySelector('[data-modal]'),
   };
 
-  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.openModalBtn.forEach(element => element.addEventListener('click', toggleModal));
+
+  // refs.openModalBtn.addEventListener('click', toggleModal);
   refs.closeModalBtn.addEventListener('click', toggleModal);
   refs.modal.addEventListener('click', handleBackdropClick);
 
   function toggleModal() {
-    document.body.classList.toggle('no-scroll');
+    // document.body.classList.toggle('no-scroll');
     refs.modal.classList.toggle('is-hidden');
     window.addEventListener('keydown', handleKeyDown);
   }
@@ -18,7 +20,7 @@
   function handleKeyDown(e) {
     if (e.code === 'Escape') {
       refs.modal.classList.add('is-hidden');
-      document.documentElement.classList.remove('no-scroll');
+      // document.documentElement.classList.toggle('no-scroll');
       window.removeEventListener('keydown', handleKeyDown);
     }
   }
@@ -26,7 +28,7 @@
   function handleBackdropClick(e) {
     if (e.target === refs.modal) {
       refs.modal.classList.toggle('is-hidden');
-      document.documentElement.classList.remove('no-scroll');
+      // document.documentElement.classList.toggle('no-scroll');
     }
   }
 })();
