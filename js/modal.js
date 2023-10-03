5fc75e912f8e46084950bd4afe2f64fbc52d297f
Valerii Pometun <vmpometun@gmail.com>
2023-10-03 13:28:09 +0300

    update modal js files

--- js/modal.js
+++ js/modal.js
@@ -7,12 +7,12 @@
 
   refs.openModalBtn.forEach(element => element.addEventListener('click', toggleModal));
 
-  // refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.openModalBtn.addEventListener('click', toggleModal);
   refs.closeModalBtn.addEventListener('click', toggleModal);
   refs.modal.addEventListener('click', handleBackdropClick);
 
   function toggleModal() {
-    // document.body.classList.toggle('no-scroll');
+    document.body.classList.toggle('no-scroll');
     refs.modal.classList.toggle('is-hidden');
     window.addEventListener('keydown', handleKeyDown);
   }
@@ -20,7 +20,7 @@
   function handleKeyDown(e) {
     if (e.code === 'Escape') {
       refs.modal.classList.add('is-hidden');
-      // document.documentElement.classList.toggle('no-scroll');
+      document.documentElement.classList.toggle('no-scroll');
       window.removeEventListener('keydown', handleKeyDown);
     }
   }
@@ -28,7 +28,7 @@
   function handleBackdropClick(e) {
     if (e.target === refs.modal) {
       refs.modal.classList.toggle('is-hidden');
-      // document.documentElement.classList.toggle('no-scroll');
+      document.documentElement.classList.toggle('no-scroll');
     }
   }
 })();
