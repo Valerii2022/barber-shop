3043ae2ab6303d37f45a8b651481e5a1fc269a3e
Valerii Pometun <vmpometun@gmail.com>
2023-10-03 10:14:16 +0300

    update README file

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# fs73
-BarberShop
+# BarberShop website
+
+Adaptive layout of all elements of the BarberShop website
+[layout](https://www.figma.com/file/J8hCiWC1HXIfHmDAScZi6P/1---Barbershop-EN---FS73?node-id=0%3A1&mode=dev) -
+a hair salon for men in Kyiv.
+
+## [Main Page](https://valerii2022.github.io/barber-shop/)
+
+![Main Desktop Page](./assets/desktop.png)
+
+## Tablet BarberShop website
+
+![Main Tablet Page](./assets/tablet.png)
+
+## Mobile BarberShop website
+
+![Main Mobile Page](./assets//mobile.png)
+
+### Mobile Menu
+
+![Mobile menu](./assets//mobile-menu.png)
--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -4,15 +4,14 @@
   const closeMenuBtn = document.querySelector('.js-close-menu');
 
   const toggleMenu = () => {
-    const isMenuOpen =
-      openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
+    const isMenuOpen = openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
     openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
     mobileMenu.classList.toggle('is-open');
 
-    const scrollLockMethod = !isMenuOpen
-      ? 'disableBodyScroll'
-      : 'enableBodyScroll';
-      bodyScrollLock[scrollLockMethod](document.body);
+    // const scrollLockMethod = !isMenuOpen
+    //   ? 'disableBodyScroll'
+    //   : 'enableBodyScroll';
+    //   bodyScrollLock[scrollLockMethod](document.body);
   };
 
   openMenuBtn.addEventListener('click', toggleMenu);
@@ -21,7 +20,7 @@
   // Close the mobile menu on wider screens if the device orientation changes
   window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
     if (!e.matches) return;
-     mobileMenu.classList.remove('is-open');
+    mobileMenu.classList.remove('is-open');
     openMenuBtn.setAttribute('aria-expanded', false);
     bodyScrollLock.enableBodyScroll(document.body);
   });
