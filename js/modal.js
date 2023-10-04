205a382c0fb273a5e3854743432f5e255d1e986e
Valerii Pometun <vmpometun@gmail.com>
2023-10-04 10:08:07 +0300

    add success window / show in console user info

--- js/modal.js
+++ js/modal.js
@@ -1,51 +1,66 @@
-(() => {
-  const refs = {
-    openModalBtn: document.querySelectorAll('[data-modal-open]'),
-    closeModalBtn: document.querySelector('[data-modal-close]'),
-    modal: document.querySelector('[data-modal]'),
-    sendBtn: document.querySelectorAll('[data-send-info]'),
-  };
+const refs = {
+  openModalBtn: document.querySelectorAll('[data-modal-open]'),
+  closeModalBtn: document.querySelector('[data-modal-close]'),
+  modal: document.querySelector('[data-modal]'),
+  sendBtn: document.querySelectorAll('[data-send-info]'),
+  successModal: document.querySelector('[data-success-modal]'),
+};
 
-  let name;
-  let phone;
-  let email;
-  let barber;
+refs.openModalBtn.forEach(element => element.addEventListener('click', toggleModal));
+refs.sendBtn.forEach(element => addEventListener('submit', handleSendSubmitBtn));
 
-  refs.openModalBtn.forEach(element => element.addEventListener('click', toggleModal));
-  refs.sendBtn.forEach(element => addEventListener('submit', handleSendSubmitBtn));
+refs.closeModalBtn.addEventListener('click', toggleModal);
+refs.modal.addEventListener('click', handleBackdropClick);
 
-  refs.closeModalBtn.addEventListener('click', toggleModal);
-  refs.modal.addEventListener('click', handleBackdropClick);
+function toggleModal() {
+  document.body.classList.toggle('no-scroll');
+  refs.modal.classList.toggle('is-hidden');
+  window.addEventListener('keydown', handleKeyDown);
+}
 
-  function toggleModal() {
-    document.body.classList.toggle('no-scroll');
-    refs.modal.classList.toggle('is-hidden');
-    window.addEventListener('keydown', handleKeyDown);
-  }
-
-  function handleKeyDown(e) {
-    if (e.code === 'Escape') {
-      refs.modal.classList.add('is-hidden');
-      window.removeEventListener('keydown', handleKeyDown);
-    }
-  }
-
-  function handleBackdropClick(e) {
-    if (e.target === refs.modal) {
-      refs.modal.classList.toggle('is-hidden');
-    }
-  }
-
-  function handleSendSubmitBtn(e) {
-    e.preventDefault();
-    name = e.target[0].value;
-    phone = e.target[1].value;
-    if (e.target[1].value) {
-      email = e.target[2].value;
-    }
-    e.target[0].value = '';
-    e.target[1].value = '';
-    e.target[2].value = '';
+function handleKeyDown(e) {
+  if (e.code === 'Escape') {
     refs.modal.classList.add('is-hidden');
+    document.body.classList.remove('no-scroll');
+    window.removeEventListener('keydown', handleKeyDown);
   }
-})();
+}
+
+function handleBackdropClick(e) {
+  if (e.target === refs.modal) {
+    refs.modal.classList.toggle('is-hidden');
+    document.body.classList.remove('no-scroll');
+  }
+}
+
+function handleSendSubmitBtn(e) {
+  e.preventDefault();
+  e.target[3].name && e.target[4].name
+    ? console.table({
+        [e.target[0].name]: e.target[0].value,
+        [e.target[1].name]: e.target[1].value,
+        [e.target[2].name]: e.target[2].value,
+        [e.target[3].name]: e.target[3]?.value,
+        [e.target[4].name]: e.target[4]?.value,
+      })
+    : console.table({
+        [e.target[0].name]: e.target[0].value,
+        [e.target[1].name]: e.target[1].value,
+        [e.target[2].name]: e.target[2].value,
+      });
+
+  e.target[0].value = '';
+  e.target[1].value = '';
+  e.target[2].value = '';
+  refs.modal.classList.add('is-hidden');
+  setTimeout(handleSuccessModalOpen, 1000);
+  setTimeout(handleSuccessModalClose, 4000);
+}
+
+function handleSuccessModalOpen() {
+  refs.successModal.classList.remove('is-hidden');
+}
+
+function handleSuccessModalClose() {
+  refs.successModal.classList.add('is-hidden');
+}
